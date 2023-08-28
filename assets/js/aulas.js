5315d2cf8d6d18a856bf6956447429814beaa250
VrBnDev <rafaelavborgess@gmail.com>
2023-08-28 19:19:25 -0300

    Aulas e desafios sendo atualizados

--- assets/js/aulas.js
+++ assets/js/aulas.js
@@ -1,30 +1,34 @@
 let aulasCadastradas = [
     {
         id : 0,
-        tema: "Tema 01",
-        conteudo: 'Conteudo 01',
-        atividade: 'Tem sim',
-        frequencia: 'Presença'
+        tema: "Arquitetura Cliente-Servidor",
+        conteudo: 'A Arquitetura Cliente-Servidor divide sistemas em cliente e servidor. O cliente requisita serviços e o servidor os fornece, através de comunicação em rede. Amplamente presente em aplicações web e bancos de dados, oferece escalabilidade, manutenção simplificada e segurança de dados melhorada. - Chat GPT',
+        atividade: 'Desafio #01 - Alterar projeto',
+        url: 'atividade-01/index.html',
+        frequencia: 'Presença ✅'
     },
     {
         id : 1,
-        tema: "",
-        conteudo: '',
-        atividade: '',
-        frequencia: ''
+        tema: "Git Pages",
+        conteudo: 'GitHub Pages é um serviço hospedagem para sites estáticos do GitHub. Permite criar sites de repositórios Git, ótimo para blogs e documentação. Fácil de configurar e personalizar, acessível via subdomínio GitHub ou domínio próprio. Ideal para compartilhar informações de maneira ágil e gratuita.',
+        atividade: 'Desafio #02 - Git Pages',
+        url: 'https://vrbndev.github.io/Introducao-Web/projeto.html',
+        frequencia: 'Presença ✅'
     },
     {
         id : 2,
-        tema: "",
-        conteudo: '',
+        tema: "Introdução a HTML - Parte I",
+        conteudo: 'Aprendendo sobre as tags',
         atividade: '',
+        url: '',
         frequencia: ''
-    },
+    }, 
     {
         id : 3,
         tema: "",
         conteudo: '',
         atividade: '',
+        url: '',
         frequencia: ''
     },
 ]
@@ -34,25 +38,36 @@ let aulasCadastradas = [
 const containerAula = document.querySelector('section');
 
 function listaAulas(){
-    
-    const aula = document.createElement('div');
-    aula.classList.add('cardAula');
-    
-    const tituloAula = document.createElement('h3');
-    tituloAula.classList.add('titulo-aula');
-    tituloAula.innerText = aulasCadastradas[0].tema;
-    aula.appendChild(tituloAula);
 
-    const conteudoAula = document.createElement('p');
-    conteudoAula.classList.add('conteudo-aula');
-    conteudoAula.innerText = aulasCadastradas[0].conteudo;
-    aula.appendChild(conteudoAula);
+    for (i=0; i < aulasCadastradas.length; i++){
+        const aula = document.createElement('div');
+        aula.classList.add('cardAula');
+        
+        const tituloAula = document.createElement('h3');
+        tituloAula.classList.add('titulo-aula');
+        tituloAula.innerText = aulasCadastradas[i].tema;
+        aula.appendChild(tituloAula);
 
-    const atividadeAula = document.createElement('p');
-    atividadeAula.classList.add('atividade-aula');
-    atividadeAula.innerText = aulasCadastradas[0].atividade;
-    aula.appendChild(atividadeAula);
+        const conteudoAula = document.createElement('p');
+        conteudoAula.classList.add('conteudo-aula');
+        conteudoAula.innerText = aulasCadastradas[i].conteudo;
+        aula.appendChild(conteudoAula);
 
+        const footerAula = document.createElement('div');
+        footerAula.classList.add('footer-aula');
+        aula.appendChild(footerAula);
 
-    containerAula.appendChild(aula);
+        const atividadeAula = document.createElement('a');
+        atividadeAula.classList.add('atividade-aula');
+        atividadeAula.innerText = aulasCadastradas[i].atividade;
+        atividadeAula.href = aulasCadastradas[i].url;
+        footerAula.appendChild(atividadeAula);
+
+        const frequenciaAtividade = document.createElement('p');
+        frequenciaAtividade.classList.add('frequencia');
+        frequenciaAtividade.innerText = aulasCadastradas[i].frequencia;
+        footerAula.appendChild(frequenciaAtividade);
+
+        containerAula.appendChild(aula);
+    }
 }
