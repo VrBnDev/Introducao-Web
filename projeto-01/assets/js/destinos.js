121bfa99d5f3a3c039627bff9cdb05e927511b3a
VrBnDev <rafaelavborgess@gmail.com>
2023-09-12 13:33:44 -0300

    Construindo página formulário

---
+++ projeto-01/assets/js/destinos.js
@@ -0,0 +1,24 @@
+const pacotesViagens = [
+    {
+        id:0,
+        destino: 'Maragogi',
+        img: 'assets/imagens/maragogi.png'
+    },
+    {
+        id:1,
+        destino: 'Trancoso',
+        img: 'assets/imagens/trancoso.png'
+    },
+    {
+        id:2,
+        destino: 'Campos do Jordão',
+        img: 'assets/imagens/campos-do-jordao.png'
+    }
+]
+
+const containerPctViagens = document.querySelector('container-pct-viagens');
+
+
+function pctViagens(){
+
+}
