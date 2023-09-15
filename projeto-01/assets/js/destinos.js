287f70ff7e0445f2dd9cd2b7f9c38228715f301b
VrBnDev <rafaelavborgess@gmail.com>
2023-09-15 13:41:55 -0300

    Criação dos cards

--- projeto-01/assets/js/destinos.js
+++ projeto-01/assets/js/destinos.js
@@ -1,8 +1,11 @@
-const pacotesViagens = [
+let pacotesViagens = [
     {
         id:0,
-        destino: 'Maragogi',
-        img: 'assets/imagens/maragogi.png'
+        destino: 'Pacote Maragogi - 2023',
+        img: 'assets/imagens/maragogi.png',
+        duracao: 'Duração: 7 dias | 6 noites',
+        incluso: ['Hospedagem à beira-mar', 'Passagens de ida e volta', 'Translado Aeroporto/Resort', 'Café da manhã no resort', 'Passeios de Buggy'],
+        preco: 'R$ 5.500,00'
     },
     {
         id:1,
@@ -16,9 +19,34 @@ const pacotesViagens = [
     }
 ]
 
-const containerPctViagens = document.querySelector('container-pct-viagens');
+const containerPctViagens = document.querySelector('.container-pacote-viagem');
 
 
 function pctViagens(){
 
+    const containerPct = document.createElement('div');
+    containerPct.classList.add('card-pacote-viagem');
+    
+    const imagemCard = document.createElement('img');
+    imagemCard.classList.add('imagem-pacote');
+    imagemCard.src = pacotesViagens[2].img;
+    
+    const tituloCard = document.createElement('h2');
+    tituloCard.classList.add('titulo-card');
+    tituloCard.innerText = pacotesViagens[2].destino;
+
+    const subtituloCard = document.createElement('h3');
+    subtituloCard.classList.add('subtitulo-card');
+    subtituloCard.innerText = pacotesViagens[0].duracao;
+    
+    const inclusoCard = document.createElement('ul');
+    inclusoCard.classList.add('subtitulo-card');
+    inclusoCard.innerText = pacotesViagens[0].incluso;
+
+    containerPct.appendChild(imagemCard);
+    containerPct.appendChild(tituloCard);
+    containerPct.appendChild(subtituloCard);
+    containerPct.appendChild(inclusoCard);
+    containerPctViagens.appendChild(containerPct);
+    
 }
