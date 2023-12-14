4e3d9240cf3a6dc5a1e50f67b982a9a20bbfccc2
VrBnDev <rafaelavborgess@gmail.com>
2023-12-13 22:12:03 -0300

    em andamento...

--- atividade-23/assets/js/atividade-js.js
+++ atividade-23/assets/js/atividade-js.js
@@ -1,4 +1,20 @@
 
 function minhaFuncao() {
-    alert("Teste js")
+    alert("Merece um dez!")
+}
+
+function raizQuadrada(num) {
+    var raiz = Math.sqrt(num) 
+    return alert(`A raiz quadrada de ${num} é ${raiz}`)
+}
+
+function contagem(n){  
+    const containerPai = document.getElementById('desafio-02') 
+ 
+    for(var inicio = 1; inicio <= n; inicio++){
+        const numero = document.createElement('span')
+        numero.classList.add('text-danger')
+        numero.innerText = `${inicio} `
+        containerPai.appendChild(numero)
+    }
 }
