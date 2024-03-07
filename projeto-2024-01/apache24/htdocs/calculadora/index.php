b2b5c763a56e3cd2f3727173dc60676b1572e4c4
VrBnDev <rafaelavborgess@gmail.com>
2024-03-07 13:50:22 -0300

    Estudos - Almoço 13h48

--- projeto-2024-01/apache24/htdocs/calculadora/index.php
+++ projeto-2024-01/apache24/htdocs/calculadora/index.php
@@ -3,15 +3,19 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+	<link rel="stylesheet" href="styles/styles.css">
 	<title>Calculadora</title>
 </head>
 <body>
-	<h1>Bem-Vindo a Calculadora</h1>
-
-	<br>
-	<hr>
-	<br>
-	<a href="controller/controller.php?funcao=formulario">Iniciar</a>
-
+	<main>
+		<h1>Bem-Vindo a Calculadora</h1>
+		<hr><br>
+		<a href="controller/controller.php?funcao=formulario">Iniciar</a>
+		<div>
+			<i class="fa-solid fa-calculator"></i>
+		</div>
+	</main>
 </body>
 </html>
